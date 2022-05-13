b76e15a80706ef58beafc402ee2c01d9924a1e50
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-05-13 21:40:08 +0000

    [dotfiles] Config backup moves whole folder under HOME

--- .install-dotfiles.sh
+++ .install-dotfiles.sh
@@ -1,45 +1,52 @@
 #!/usr/bin/env bash
 
-if ! type git >/dev/null
-then
-	echo "Error: git is not installed, aborting."
-	exit
-fi
+# Test me
+type git >/dev/null 2>&1 ||
+{
+	echo >&2 "Error: git is not installed, aborting."
+	exit 1
+}
 
 dotfiles_name=.dotfiles
 dotfiles_dir=${HOME}/${dotfiles_name}
 dotfiles_backupdir=${HOME}/.dotfiles-backup
 
-function dotfiles {
+function dotfiles
+{
 	git --git-dir=${dotfiles_dir} --work-tree=${HOME} $@
 }
 
-function mkdir_mv {
-	local dir=$(dirname "./${2}")
-	echo "Backing up: ${1}";
-	mkdir -p -- ${dir}
-	mv -- ${1} ${2}
-}
-export -f mkdir_mv
+function backup_file
+{
+	while read line; do
+		echo "Backing up: ${line}"
 
-echo "Installing Dotfiles."
+		local dir=$(dirname "${line}" | sed "s|${HOME}|${dotfiles_backupdir}|g")
+		mkdir -p -- "${dotfiles_backupdir}/${dir}"
+		mv -- ${line} ${dotfiles_backupdir}
+	done < /dev/stdin
+}
+
+echo "Setting up dotfiles."
+
 cd ${HOME}
 git clone --bare https://github.com/Abstract-Everything/.dotfiles ${dotfiles_name} --quiet
 
-dotfiles checkout --quiet &>/dev/null
-if [ ${?} = 0 ]; then
-	echo "Checked out dotfiles.";
-else
-	mkdir -p ${dotfiles_backupdir}
-	echo 'Backing up pre-existing dotfiles.';
-	dotfiles checkout 2>&1	\
-	| egrep "\s+\."		\
-	| awk {'print $1'}	\
-	| xargs -I{} bash -c "mkdir_mv {} ${dotfiles_backupdir}"
+dotfiles ls-tree --full-tree -r HEAD                                                                  \
+| cut -f 2                                                                                            \
+| grep "\.config"                                                                                     \
+| sed -E 's|.config/([^/]+).*$|\1|g'                                                                  \
+| uniq                                                                                                \
+| xargs -I{} find ${HOME} -not -path "${dotfiles_backupdir}/*" -regextype sed -regex ".*/\.\?{}[^/]*" \
+| tac                                                                                                 \
+| backup_file
 
-	echo 'Backup done.';
-fi;
+dotfiles ls-tree --full-tree -r HEAD                                    \
+| cut -f 2                                                              \
+| grep -v "\.config"                                                    \
+| xargs -I{} find ${HOME} -not -path "${dotfiles_backupdir}/*" -name {} \
+| backup_file
 
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
-echo 'Dotfiles installed.';
+echo 'Dotfiles setup successful.';
