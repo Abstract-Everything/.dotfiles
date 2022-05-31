310a35543bdb5d9e3c10a4c9b62caa58ba72169f
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-05-31 21:44:13 +0200

    [nvim] Telescope shortcuts to search git tracked or all files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -144,9 +144,8 @@ telescope.load_extension 'fzf'
 
 --Add leader shortcuts
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
-vim.keymap.set('n', '<leader>sf', function()
-  require('telescope.builtin').find_files { previewer = false }
-end)
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').git_files)
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files)
 vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find)
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags)
 vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
