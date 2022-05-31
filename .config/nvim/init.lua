c169f73d3be8abfa0f83afa813576f57d6269396
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-05-31 13:39:19 +0200

    [nvim] Use wrap friendly j,k and highlight on yank

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -90,6 +90,20 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+--Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
 --- Clear highlight on esc
 vim.api.nvim_set_keymap('n', '<esc>', ':noh<return>', silent_noremap)
 
