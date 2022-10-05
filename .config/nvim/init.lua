edcadd75b63c5ae9524dd4ae62beb209e2a6d4dc
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-10-05 18:55:34 +0200

    [nvim] Remove j to gj and k to gk remaps
    It is more useful to traverse by lines and use / or f to traverse
    horizontally than using these remaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -119,10 +119,6 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
---Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
