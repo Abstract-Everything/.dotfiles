c45c9bac8cb8c9dcbde3cffbf986997b9267eeea
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-10-05 19:14:19 +0200

    [nvim] Enable Treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,6 +50,10 @@ require('packer').startup(function(use)
 		]]
 	}
 
+	-- Navigating code
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use 'nvim-treesitter/nvim-treesitter-textobjects'
+
 	--- Tools for writing code
 	use 'neovim/nvim-lspconfig'
 	use 'hrsh7th/nvim-cmp'
@@ -104,7 +108,6 @@ vim.o.breakindent = true
 vim.o.colorcolumn = '81'
 vim.o.updatetime = 250
 vim.o.completeopt = 'menu,menuone,noselect'
-vim.o.foldclose = 'all'
 
 --- Status
 vim.o.signcolumn = 'yes'
@@ -188,6 +191,61 @@ vim.keymap.set('n', '<leader>so', function()
 	require('telescope.builtin').tags { only_current_buffer = true }
 end)
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles)
+
+-- Treesitter
+require('nvim-treesitter.configs').setup {
+	highlight = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = 'gnn',
+			node_incremental = 'grn',
+			scope_incremental = 'grc',
+			node_decremental = 'grm',
+		},
+	},
+	indent = {
+		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				['af'] = '@function.outer',
+				['if'] = '@function.inner',
+				['ac'] = '@class.outer',
+				['ic'] = '@class.inner',
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				[']m'] = '@function.outer',
+				[']]'] = '@class.outer',
+			},
+			goto_next_end = {
+				[']M'] = '@function.outer',
+				[']['] = '@class.outer',
+			},
+			goto_previous_start = {
+				['[m'] = '@function.outer',
+				['[['] = '@class.outer',
+			},
+			goto_previous_end = {
+				['[M'] = '@function.outer',
+				['[]'] = '@class.outer',
+			},
+		},
+	},
+}
+
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 
 ---- Language server protocol
 function format_range_operator(...)
