18e4db7090b916375253dafb9182475a84f29389
Jonathan Camilleri <jon.d.cam@gmail.com>
2023-06-15 16:34:23 +0200

    [setup] fix installation script

--- .install-dotfiles.sh
+++ .install-dotfiles.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Test me
 type git >/dev/null 2>&1 ||
 {
 	echo >&2 "Error: git is not installed, aborting."
@@ -16,37 +15,48 @@ function dotfiles
 	git --git-dir=${dotfiles_dir} --work-tree=${HOME} $@
 }
 
-function backup_file
+function backup_existing_files
 {
 	while read line; do
+
+		if [ ! -d "${line}" ] && [ ! -f "${line}" ]; then
+			continue
+		fi
+
 		echo "Backing up: ${line}"
 
-		local dir=$(dirname "${line}" | sed "s|${HOME}|${dotfiles_backupdir}|g")
-		mkdir -p -- "${dotfiles_backupdir}/${dir}"
-		mv -- ${line} ${dotfiles_backupdir}
+		local dir=$(echo "${line}" | sed "s|${HOME}|${dotfiles_backupdir}|g")
+		local backup_dir="${dotfiles_backupdir}/${dir}"
+
+		mkdir -p -- "${backup_dir}"
+		mv -- ${line} "${backup_dir}"
 	done < /dev/stdin
 }
 
 echo "Setting up dotfiles."
 
-cd ${HOME}
+pushd ${HOME} > /dev/null
+if [ -d "${dotfiles_name}" ]; then
+	echo "Fatal: '${HOME}/.dotfiles' already exists. Please rename it in order to continue"
+	exit 1
+fi
+
 git clone --bare https://github.com/Abstract-Everything/.dotfiles ${dotfiles_name} --quiet
 
-dotfiles ls-tree --full-tree -r HEAD                                                                  \
-| cut -f 2                                                                                            \
-| grep "\.config"                                                                                     \
-| sed -E 's|.config/([^/]+).*$|\1|g'                                                                  \
-| uniq                                                                                                \
-| xargs -I{} find ${HOME} -not -path "${dotfiles_backupdir}/*" -regextype sed -regex ".*/\.\?{}[^/]*" \
-| tac                                                                                                 \
-| backup_file
+dotfiles ls-tree --full-tree -r HEAD                                           \
+| cut -f 2                                                                     \
+| grep "\.config"                                                              \
+| sed -E 's|(.config/([^/]+)).*$|\1|g'                                         \
+| uniq                                                                         \
+| backup_existing_files
 
-dotfiles ls-tree --full-tree -r HEAD                                    \
-| cut -f 2                                                              \
-| grep -v "\.config"                                                    \
-| xargs -I{} find ${HOME} -not -path "${dotfiles_backupdir}/*" -name {} \
-| backup_file
+dotfiles ls-tree --full-tree -r HEAD                                           \
+| cut -f 2                                                                     \
+| grep -v "\.config"                                                           \
+| backup_existing_files
 
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
+popd > /dev/null
+
 echo 'Dotfiles setup successful.';
