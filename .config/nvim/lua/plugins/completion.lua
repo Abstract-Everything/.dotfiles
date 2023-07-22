4b647229cfdddf1c749014ec1a811e0118f95ec7
Jonathan Camilleri <jon.d.cam@gmail.com>
2023-07-22 17:49:56 +0200

    [nvim] add autocompletion for cmdline

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -3,6 +3,7 @@ return {
   "hrsh7th/cmp-calc",
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-nvim-lsp-signature-help",
   "hrsh7th/cmp-nvim-lua",
@@ -70,6 +71,21 @@ return {
           },
         },
       }
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources {
+          { name = "path" },
+          { name = "cmdline" },
+        },
+      })
     end,
   },
 }
