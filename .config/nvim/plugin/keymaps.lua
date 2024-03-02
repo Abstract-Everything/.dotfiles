50f3ea06fe359a577980e182987be50cee334358
Jonathan Camilleri <jon.d.cam@gmail.com>
2024-03-02 21:30:39 +0100

    [nvim] automatically resize splits if window resized

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -24,6 +24,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = create_augroup "resize_splits",
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd "tabdo wincmd ="
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
 vim.keymap.set("n", "<leader>e", function()
   vim.diagnostic.open_float()
 end, silent_noremap)
