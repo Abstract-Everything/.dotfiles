e06ec219b6ab05f4eb23a82a8c96ab7ff56c4ac6
Jonathan Camilleri <jon.d.cam@gmail.com>
2024-03-02 13:55:51 +0100

    [nvim] update packages

--- .config/nvim/lua/plugins/languages/specific/bash.lua
+++ .config/nvim/lua/plugins/languages/specific/bash.lua
@@ -4,7 +4,6 @@ return {
     opts = function(_, options)
       local nls = require "null-ls"
       options.sources = vim.list_extend(options.sources or {}, {
-        nls.builtins.code_actions.shellcheck,
         nls.builtins.formatting.shfmt,
       })
     end,
