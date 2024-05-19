3c2caa894f4c7852e7ac01fbaf54b90b2f2dbd9d
Jonathan Camilleri <jon.d.cam@gmail.com>
2024-05-19 13:30:42 +0200

    feat(nvim): setup codelldb for debugging

--- .config/nvim/lua/plugins/languages/specific/cpp.lua
+++ .config/nvim/lua/plugins/languages/specific/cpp.lua
@@ -23,4 +23,56 @@ return {
       table.insert(opts.sorting.comparators, 1, require "clangd_extensions.cmp_scores")
     end,
   },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = {
+        "codelldb",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      local dap = require "dap"
+      if not dap.adapters["codelldb"] then
+        require("dap").adapters.codelldb = {
+          type = "server",
+          host = "localhost",
+          port = "${port}",
+          executable = {
+            command = "codelldb",
+            args = {
+              "--port",
+              "${port}",
+            },
+          },
+        }
+      end
+
+      local configuration = {
+        {
+          type = "codelldb",
+          request = "launch",
+          name = "Launch file",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "codelldb",
+          request = "attach",
+          name = "Attach to process",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+        },
+      }
+      dap.configurations.c = configuration
+      dap.configurations.cpp = configuration
+    end,
+  },
 }
