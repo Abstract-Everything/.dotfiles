5483ab686cbb13b1be58fdc84f38b26a9e2ca62a
Jonathan Camilleri <jon.d.cam@gmail.com>
2024-09-15 09:53:49 +0200

    nvim: add telescope-ui-select

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
         build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
         config = function() end,
       },
+      { "nvim-telescope/telescope-ui-select.nvim" },
     },
     -- Add more keys - such as quickfix history
     keys = {
@@ -46,6 +47,7 @@ return {
 
       telescope.setup(opts)
       telescope.load_extension "fzf"
+      telescope.load_extension "ui-select"
     end,
   },
 }
