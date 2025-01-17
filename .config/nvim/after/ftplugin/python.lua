4078d266ca6b8e25c61cd26e88fef41b1f6f4ecf
Jonathan Camilleri <jon.d.cam@gmail.com>
2025-01-17 23:37:21 +0100

    [nvim] move python config into after/ftplugin

---
+++ .config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,86 @@
+local Config = require "config"
+
+local PATH_SEPARATOR = ":"
+
+vim.fn.assert_equal(0, vim.fn.has "win32", "This currently does not support windows")
+
+--#region dynamic venv change
+local function read_current_bin_path()
+  for _, path in ipairs(vim.fn.split(vim.env.PATH, PATH_SEPARATOR)) do
+    if vim.fn.executable(vim.fs.joinpath(path, "python3")) == 1 then
+      return path
+    end
+  end
+
+  return nil
+end
+
+local function find_venv(buffer_path)
+  local venv_dir = vim.fs.find(".venv", {
+    upward = true,
+    type = "directory",
+    path = vim.fs.dirname(buffer_path),
+  })
+
+  if #venv_dir > 0 then
+    return venv_dir[1]
+  end
+
+  return nil
+end
+
+local function update_venv()
+  local buffer_path = vim.api.nvim_buf_get_name(0)
+  local venv_path = find_venv(buffer_path)
+  if not venv_path then
+    vim.notify("venv not found from: " .. buffer_path)
+    return
+  end
+
+  local bin_path = vim.fs.joinpath(venv_path, "bin")
+  local python_path = vim.fs.joinpath(bin_path, "python3")
+
+  if vim.fn.executable(python_path) == 0 then
+    vim.notify("python3 executable not found at: " .. python_path)
+    return
+  end
+
+  require("dap-python").resolve_python = function()
+    return python_path
+  end
+
+  local current_system_path = vim.env.PATH
+  local current_bin_path = read_current_bin_path()
+  if current_bin_path then
+    if Config.debug_logs == true then
+      vim.notify("Removing current binary path: '" .. current_bin_path .. "'")
+    end
+    current_system_path = string.gsub(current_system_path, current_bin_path, "")
+  end
+  local system_path = bin_path .. PATH_SEPARATOR .. current_system_path
+
+  if Config.debug_logs == true then
+    vim.notify("Activating '" .. venv_path .. "' with binary path '" .. system_path .. "'")
+  end
+
+  local client = vim.lsp.get_clients({ name = "pyright" })[1]
+  if client then
+    client.settings.python.pythonPath = python_path
+    client.notify "workspace/didChangeConfiguration"
+  else
+    vim.notify "pyright is not active"
+  end
+
+  vim.env.PATH = system_path
+  vim.env.VIRTUAL_ENV = venv_path
+end
+--#endregion
+
+local buffer = vim.api.nvim_get_current_buf()
+vim.api.nvim_create_autocmd({ "BufEnter", "LspAttach" }, {
+  buffer = buffer,
+  callback = update_venv,
+})
+
+vim.keymap.set("n", "<leader>dT", require("dap-python").test_method, Config.keymaps.silent_noremap)
+vim.keymap.set("n", "<leader>dC", require("dap-python").test_class, Config.keymaps.silent_noremap)
--- .config/nvim/lua/plugins/languages/specific/python.lua
+++ .config/nvim/lua/plugins/languages/specific/python.lua
@@ -1,42 +1,9 @@
-local Config = require "config"
-
-local last_root_directory = ""
-
-local function find_python_client_ids()
-  local ids = {}
-  for _, client in ipairs(vim.lsp.get_active_clients()) do
-    if client.name == "pyright" then
-      table.insert(ids, client.id)
-    end
-  end
-  return ids
-end
-
-local function automatically_set_parent_venv()
-  local root_directory = Config.root { lsp_client_ids = find_python_client_ids() }
-  if not root_directory then
-    return
-  end
-
-  if root_directory == last_root_directory then
-    vim.notify("LspRoot is the same, not changing venv: " .. root_directory, vim.log.levels.TRACE)
-    return
-  end
-  last_root_directory = root_directory
-
-  local cwd = vim.fn.getcwd()
-  vim.api.nvim_set_current_dir(root_directory)
-  require("venv-selector").retrieve_from_cache()
-  vim.api.nvim_set_current_dir(cwd)
-end
-
 return {
   {
     "nvimtools/none-ls.nvim",
     opts = function(_, options)
       local nls = require "null-ls"
       options.sources = vim.list_extend(options.sources or {}, {
-        nls.builtins.formatting.black,
         nls.builtins.diagnostics.mypy,
       })
     end,
@@ -45,25 +12,8 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- TODO: Check configuration
         pyright = {},
         ruff = {},
-      },
-      setup = {
-        pyright = function(options)
-          vim.api.nvim_create_autocmd({ "LspAttach" }, {
-            callback = function()
-              automatically_set_parent_venv()
-            end,
-          })
-          vim.api.nvim_create_autocmd({ "BufEnter" }, {
-            callback = function()
-              automatically_set_parent_venv()
-            end,
-          })
-          -- TODO: Add option to let setup be handled by init.lua
-          require("lspconfig").pyright.setup(options)
-        end,
       },
     },
   },
@@ -73,25 +23,6 @@ return {
       "mfussenegger/nvim-dap",
       "jay-babu/mason-nvim-dap.nvim",
     },
-    dev = true,
-    keys = {
-      {
-        "<leader>dT",
-        function()
-          require("dap-python").test_method()
-        end,
-        desc = "Debug Method",
-        ft = "python",
-      },
-      {
-        "<leader>dC",
-        function()
-          require("dap-python").test_class()
-        end,
-        desc = "Debug Class",
-        ft = "python",
-      },
-    },
     config = function()
       local path = require("mason-registry").get_package("debugpy"):get_install_path()
       require("dap-python").setup(path .. "/venv/bin/python")
@@ -100,23 +31,5 @@ return {
         return require("venv-selector").get_active_path()
       end
     end,
-  },
-  {
-    "linux-cultist/venv-selector.nvim",
-    cmd = "VenvSelect",
-    opts = function(_, options)
-      return vim.tbl_deep_extend("force", options, {
-        name = {
-          "venv",
-          ".venv",
-          "env",
-          ".env",
-        },
-      })
-    end,
-    keys = {
-      { "<leader>vs", "<cmd>VenvSelect<cr>" },
-      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-    },
   },
 }
