b2a385ffb155f81f7d22398b8e1f143c59c2491c
Jonathan Camilleri <jon.d.cam@gmail.com>
2025-01-17 14:43:53 +0100

    [nvim] add selene linter

--- .config/nvim/lua/plugins/languages/specific/luau.lua
+++ .config/nvim/lua/plugins/languages/specific/luau.lua
@@ -5,6 +5,7 @@ return {
       local nls = require "null-ls"
       options.sources = vim.list_extend(options.sources or {}, {
         nls.builtins.formatting.stylua,
+        nls.builtins.diagnostics.selene,
       })
     end,
   },
