42e37a66492bf0f382c978cc976f8b2cde206a2d
Jonathan Camilleri <jon.d.cam@gmail.com>
2025-01-18 14:20:40 +0100

    [nvim] simplify treesitter configuration

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,86 +1,79 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
     build = ":TSUpdate",
-    keys = {
-      { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Decrement selection", mode = "x" },
-    },
   },
   "nvim-treesitter/nvim-treesitter-context",
   "nvim-treesitter/playground",
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = { "query" },
-        modules = {},
-        ignore_install = {},
-        auto_install = true,
-        sync_install = false,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
+    opts = {
+      ensure_installed = { "query" },
+      modules = {},
+      ignore_install = {},
+      auto_install = true,
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          scope_incremental = "<C-space>",
+          node_decremental = "<bs>",
         },
-        incremental_selection = {
+      },
+      textobjects = {
+        select = {
           enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
           keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["as"] = "@class.outer",
+            ["is"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["ap"] = "@conditional.outer",
+            ["ip"] = "@conditional.inner",
           },
         },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["as"] = "@class.outer",
-              ["is"] = "@class.inner",
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["ap"] = "@conditional.outer",
-              ["ip"] = "@conditional.inner",
-            },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]s"] = "@class.outer",
+            ["]a"] = "@parameter.outer",
+            ["]p"] = "@conditional.outer",
           },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]s"] = "@class.outer",
-              ["]a"] = "@parameter.outer",
-              ["]p"] = "@conditional.outer",
-            },
-            goto_next_end = {
-              ["]F"] = "@function.outer",
-              ["]S"] = "@class.outer",
-              ["]A"] = "@parameter.outer",
-              ["]P"] = "@conditional.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[s"] = "@class.outer",
-              ["[a"] = "@parameter.outer",
-              ["[p"] = "@conditional.outer",
-            },
-            goto_previous_end = {
-              ["[F"] = "@function.outer",
-              ["[S"] = "@class.outer",
-              ["[A"] = "@parameter.outer",
-              ["[P"] = "@conditional.outer",
-            },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]S"] = "@class.outer",
+            ["]A"] = "@parameter.outer",
+            ["]P"] = "@conditional.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[s"] = "@class.outer",
+            ["[a"] = "@parameter.outer",
+            ["[p"] = "@conditional.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[S"] = "@class.outer",
+            ["[A"] = "@parameter.outer",
+            ["[P"] = "@conditional.outer",
           },
         },
-      }
+      },
+    },
+    config = function(_, options)
+      require("nvim-treesitter.configs").setup(options)
     end,
   },
 }
