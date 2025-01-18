1b89e15735d4c24612bc34f49f84affa5031eedb
Jonathan Camilleri <jon.d.cam@gmail.com>
2025-01-18 13:21:42 +0100

    [nvim] remove unneeded module

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,13 +1,8 @@
 ---@class Config
----@field root config.Root
----@field telescope config.Telescope
----@field plugins config.Plugins
----@field formatting config.Formatting
 local M = {}
 
 M.root = require "config.root"
 M.telescope = require "config.telescope"
-M.plugins = require "config.plugins"
 M.formatting = require "config.formatting"
 M.keymaps = require "config.keymaps"
 M.lsp = require "config.lsp"
