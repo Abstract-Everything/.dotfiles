1fcbcaae2505e148c6153a52425abedf8dae8928
Jonathan Camilleri <jon.d.cam@gmail.com>
2025-01-29 00:00:45 +0100

    nix: setup zsh in nix

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Jonathan dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+
+      nixosConfigurations.jon =
+        nixpkgs.lib.nixosSystem
+          {
+            system = " x86_64-linux ";
+            modules = [
+              ./configuration.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                };
+                home-manager.users.jon = import ./home.nix;
+              }
+            ];
+          };
+
+      homeConfigurations.jon = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          (import ./home.nix)
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,66 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "24.11";
+
+    username = "jon";
+    homeDirectory = "/home/jon";
+
+    packages = with pkgs; [
+      zsh
+    ];
+  };
+
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
+    # shell
+    zsh = {
+      enable = true;
+      dotDir = ".config/zsh";
+      syntaxHighlighting.enable = true;
+      autosuggestion.enable = true;
+      autocd = true;
+      enableCompletion = true;
+      initExtraBeforeCompInit = ''
+        HYPHEN_INSENSITIVE="true"
+        COMPLETION_WAITING_DOTS="true"
+      '';
+      initExtra = ''
+        bindkey '^ ' autosuggest-accept
+
+        # Allows fuzzy matching of completions in the presence of typos
+        zstyle ':completion:*' completer _complete _match _approximate
+        zstyle ':completion:*:match:*' original only
+        zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+
+        # Do not suggest the ./ and ../$(current-directory)
+        zstyle ':completion:*:cd:*' ignore-parents parent pwd
+      '';
+      shellAliases = {
+        g = "git";
+        dg = "git --git-dir=$HOME/dotfiles/";
+        updatedb = "updatedb --require-visibility 0 -o $HOME/.cache/locate.db";
+        locate = "locate --database=$HOME/.cache/locate.db";
+      };
+      oh-my-zsh = {
+        enable = true;
+        theme = "af-magic";
+        plugins = [ "vi-mode" "fzf" "zoxide" "command-not-found" ];
+        extraConfig = ''
+          DISABLE_UNTRACKED_FILES_DIRTY="true"
+        '';
+      };
+    };
+  };
+}
