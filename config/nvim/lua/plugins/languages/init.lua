10081c97de56b1a31fe47d640a7e98c7c8ee6631
Jonathan Camilleri <jon.d.cam@gmail.com>
2025-07-20 10:52:12 +0200

    nvim: remove mason

--- config/nvim/lua/plugins/languages/init.lua
+++ config/nvim/lua/plugins/languages/init.lua
@@ -9,11 +9,5 @@ return {
       require("config").lsp.setup(options)
     end,
   },
-  {
-    "williamboman/mason.nvim",
-    config = function(_, options)
-      require("mason").setup(options)
-    end,
-  },
   { import = "plugins.languages.specific" },
 }
