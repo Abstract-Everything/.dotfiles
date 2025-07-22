26ee21b7effb208b21a61378ec48219f11a39bd2
Jonathan Camilleri <jon.d.cam@gmail.com>
2025-07-22 16:26:01 +0200

    [nvim]: call member function with ':' instead of '.'

--- config/nvim/lua/config/formatting.lua
+++ config/nvim/lua/config/formatting.lua
@@ -6,12 +6,13 @@ function M.format_file(async)
   if async == nil then
     async = true
   end
+
   vim.lsp.buf.format {
     async = async,
     -- Use the null-ls formatter if it has one
     filter = function(filter_client)
       for _, client in ipairs(vim.lsp.get_clients()) do
-        if client.name == "null-ls" and client.supports_method "textDocument/formatting" then
+        if client.name == "null-ls" and client:supports_method "textDocument/formatting" then
           return filter_client.name == "null-ls"
         end
       end
