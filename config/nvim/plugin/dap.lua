59e9143676bd284972b798196c70ca8e222e3156
Jonathan Camilleri <jon.d.cam@gmail.com>
2026-01-19 23:10:43 +0100

    [nvim]: add more DAP keymappings

--- config/nvim/plugin/dap.lua
+++ config/nvim/plugin/dap.lua
@@ -6,10 +6,6 @@ vim.keymap.set("n", "<leader>ds", function()
   require("dap").continue()
 end)
 
-vim.keymap.set({ "n", "v" }, "<leader>de", function()
-  dapui.eval()
-end)
-
 vim.keymap.set("n", "<leader>dr", function()
   dap.run_last()
 end)
@@ -20,6 +16,10 @@ end)
 
 vim.keymap.set("n", "<leader>du", function()
   dapui.toggle {}
+end)
+
+vim.keymap.set({ "n", "v" }, "<M-e>", function()
+  dapui.eval()
 end)
 
 vim.keymap.set("n", "<M-b>", function()
@@ -44,6 +44,14 @@ end)
 
 vim.keymap.set("n", "<M-k>", function()
   dap.step_out()
+end)
+
+vim.keymap.set("n", "<M-u>", function()
+  dap.up()
+end)
+
+vim.keymap.set("n", "<M-d>", function()
+  dap.down()
 end)
 
 dap.adapters.lldb = {
