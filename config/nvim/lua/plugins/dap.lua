19d9b96625e7c11c0a9fe4cdfd41c18d51f3f102
Jonathan Camilleri <jon.d.cam@gmail.com>
2026-01-19 23:09:39 +0100

    [nvim]: improve dap-ui layout

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -9,6 +9,51 @@ return {
       "mfussenegger/nvim-dap",
     },
     -- Without this line, config is not called by lazy automatically
-    opts = {},
+    opts = {
+      element_mappings = {
+        stacks = {
+          open = "<CR>",
+        },
+      },
+      layouts = {
+        {
+          elements = {
+            -- These seem to be inverted, so breakpoints is last
+            {
+              id = "breakpoints",
+              size = 0.25,
+            },
+            {
+              id = "stacks",
+              size = 0.25,
+            },
+            {
+              id = "scopes",
+              size = 0.25,
+            },
+            {
+              id = "watches",
+              size = 0.25,
+            },
+          },
+          position = "left",
+          size = 0.33,
+        },
+        {
+          elements = {
+            {
+              id = "repl",
+              size = 0.5,
+            },
+            {
+              id = "console",
+              size = 0.5,
+            },
+          },
+          position = "bottom",
+          size = 0.25,
+        },
+      },
+    },
   },
 }
